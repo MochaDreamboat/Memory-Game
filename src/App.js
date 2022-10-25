e47ac2a35bce549ef63256c485e74753bf50bf45
mochadreamboat <jersotmac@gmail.com>
2022-10-24 20:37:52 -0500

    Wrapped cards in flex container.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,33 @@ function App() {
         <h1>MEMORY GAME</h1>
         <p>Don't click a card more than once!</p>
       </header>
-      <div class="card">
-        <img src={require('./images/ashe.png')} alt="ashe" />
-        <h3>ASHE</h3>
-      </div>
+      {/* TO-DO: Split draft into respective components */}
+      <section id="cards">
+        <div class="card">
+          <img src={require('./images/ashe.png')} alt="ashe" />
+          <h3>ASHE</h3>
+        </div>
+        <div class="card">
+          <img src={require('./images/ana.png')} alt="ana" />
+          <h3>ANA</h3>
+        </div>
+        <div class="card">
+          <img src={require('./images/baptiste.png')} alt="ana" />
+          <h3>BAPTISTE</h3>
+        </div>
+        <div class="card">
+          <img src={require('./images/baptiste.png')} alt="ana" />
+          <h3>BAPTISTE</h3>
+        </div>
+        <div class="card">
+          <img src={require('./images/baptiste.png')} alt="ana" />
+          <h3>BAPTISTE</h3>
+        </div>
+        <div class="card">
+          <img src={require('./images/baptiste.png')} alt="ana" />
+          <h3>BAPTISTE</h3>
+        </div>
+      </section>
     </main>
   );
 }
