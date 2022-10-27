765ae6445cda63b74c554e6188ade5edc42b19c3
mochadreamboat <jersotmac@gmail.com>
2022-10-27 00:06:27 -0500

    Changed object path for accessing card name and image url.

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,16 +2,13 @@ import { React, useState } from "react";
 
 // Takes in card info from cards data structure (array?) and renders w/ event listeners on it.
 function Card (props) {
-    let { name, img } = props;
+    let { card } = props;
     return (
         <div className="card">
-          <img src={require(`${img}`)} alt="ashe" />
-          <h3>{name.toUpperCase()}</h3>
+          <img src={require(`../images/${card.image}`)} alt="ashe" />
+          <h3>{card.name.toUpperCase()}</h3>
         </div>
     )
 }
 
-let card = {
-    name: 'Ashe',
-    img: './images/ashe.png',
-};
+export default Card;
