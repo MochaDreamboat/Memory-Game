c6ab5d7935195f6e9b80ef49d2145a468c3e7731
mochadreamboat <jersotmac@gmail.com>
2022-10-26 23:41:45 -0500

    Added comment. Changed class to className.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 import { React, useState } from "react";
 
+// Takes in card info from cards data structure (array?) and renders w/ event listeners on it.
 function Card (props) {
     let { name, img } = props;
     return (
-        <div class="card">
+        <div className="card">
           <img src={require(`${img}`)} alt="ashe" />
           <h3>{name.toUpperCase()}</h3>
         </div>
