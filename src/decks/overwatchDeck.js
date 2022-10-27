cb5aca6f951fc57d37a8b172f5c2694c73e387a0
mochadreamboat <jersotmac@gmail.com>
2022-10-27 00:06:59 -0500

    Added object holding card info. Exported and imported into App.js.

---
+++ src/decks/overwatchDeck.js
@@ -0,0 +1,21 @@
+let deck = {
+    1: {
+        name: 'Ashe',
+        image: 'ashe.png',
+        picked: false
+    },
+
+    2: {
+        name: 'Ana',
+        image: 'ana.png',
+        picked: false
+    },
+
+    3: {
+        name: 'Baptiste',
+        image: 'baptiste.png',
+        picked: false
+    }
+};
+
+export default deck;
