88cb7fe7ac526c5f188cb5bcff059b80f3862189
mochadreamboat <jersotmac@gmail.com>
2022-10-27 23:05:39 -0500

    Added functionality for card selection. Selecting card adds to score if previously unpicked and resets score if already picked.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   let [score, setScore] = useState(0);
   let [bestScore, setBestScore] = useState(0);
-  let [cards, arrangeCards] = useState(deck);
+  let [cards, modifyCards] = useState(deck);
 
   // Updates best score if current score exceeds best score.
   useEffect(() => {
@@ -14,6 +14,32 @@ function App() {
       setBestScore(score);
     }
   }, [score, bestScore]);
+
+  const selectCard = (card) => {
+    if (cards[card].picked == true) {
+      setScore(0);
+      // Needs to shuffle here
+      return
+    }
+    setScore(score + 1);
+    modifyCards({
+      ...cards,
+      [card]: {
+        ...cards[card],
+        picked: true
+      }
+    })
+    // Shuffle should happen here too
+  };
+
+  // Incorporate via Fisher-Yates
+  const shuffle = () => {
+
+  };
+
+  const resetCards = () => {
+
+  }
 
   return (
     <main>
@@ -25,7 +51,7 @@ function App() {
       {/* TO-DO: Split draft into respective components */}
       <section id="cards">
         {Object.keys(cards).map((card) => {
-          return <Card card={cards[card]} key={card} />
+          return <Card card={cards[card]} key={card} select={() => selectCard(card)} />
         })}
       </section>
     </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import { React, useState } from "react";
 
 // Takes in card info from cards data structure (array?) and renders w/ event listeners on it.
 function Card (props) {
-    let { card, key } = props;
+    let { card, select } = props;
     return (
-        <div className="card" key={key}>
+        <div className="card" onClick={select}>
           <img src={require(`../images/${card.image}`)} alt="ashe" />
           <h3>{card.name.toUpperCase()}</h3>
         </div>
