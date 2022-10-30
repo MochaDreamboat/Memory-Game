4d4504eec5b44425e5b72a814d51be73e1a7a230
mochadreamboat <jersotmac@gmail.com>
2022-10-29 21:40:25 -0500

    Implemented shuffle following initial render and after every selection of unpicked card.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,65 @@
-import {React, useEffect, useState } from 'react';
+import { React, useEffect, useState, useCallback } from "react";
 import Card from "./components/Card.js";
 import deck from "./decks/overwatchDeck.js";
 
 function App() {
-
   let [score, setScore] = useState(0);
   let [bestScore, setBestScore] = useState(0);
   let [cards, modifyCards] = useState(deck);
 
   const deckKeys = Object.keys(cards);
+  let [order, changeOrder] = useState(deckKeys);
+  let [gameDidStart, changeStart] = useState(false);
 
-  // Updates best score if current score exceeds best score.
+  const shuffle = useCallback(() => {
+    const deckCopy = order;
+    for (let i = 0; i < deckCopy.length - 2; i++) {
+      let j = Math.floor(Math.random() * (deckCopy.length - i) + i); // Generate random integer between and including in and j.
+      let cardOne = deckCopy[i];
+      let cardTwo = deckCopy[j];
+
+      deckCopy[i] = cardTwo;
+      deckCopy[j] = cardOne;
+      changeOrder(deckCopy);
+      }
+      }, [order]);
+
+  useEffect(() => {
+    changeStart(true);
+  }, []);
+
+  useEffect(() => {
+    if (gameDidStart) {
+      shuffle();
+    }
+  }, [gameDidStart, shuffle]);
+
+  // Updates best score if current score exceeds current best score.
   useEffect(() => {
     if (score > bestScore) {
       setBestScore(score);
     }
-  }, [score, bestScore]);
+    shuffle();
+  }, [score, bestScore, shuffle]);
 
   const selectCard = (card) => {
     if (cards[card].picked === true) {
       setScore(0);
-      // Needs to shuffle here
-      return
+      return;
     }
     setScore(score + 1);
     modifyCards({
       ...cards,
       [card]: {
         ...cards[card],
-        picked: true
-      }
-    })
-    // Shuffle should happen here too
+        picked: true,
+      },
+    });
   };
 
-  // Incorporate via Fisher-Yates
-  const shuffle = () => {
-    for (let i = 0; i < deckKeys.length - 2; i++) {
-      let j = Math.floor(Math.random() * (deckKeys.length - i) + i) // Generate random integer between and including in and j.
-      let cardOne = deckKeys[i];
-      let cardTwo = deckKeys[j];
+  // Must reset every card in deck to unpicked.
+  const resetCards = () => {};
 
-      deckKeys[i] = cardTwo;
-      deckKeys[j] = cardOne;
-    }
-  };
-
-  const resetCards = () => {
-
-  }
   return (
     <main>
       <header>
@@ -57,10 +68,15 @@ function App() {
         <p>SCORE: {score}</p>
         <p>BEST SCORE: {bestScore}</p>
       </header>
-      {/* TO-DO: Split draft into respective components */}
       <section id="cards">
-        {deckKeys.map((card) => {
-          return <Card card={cards[card]} key={card} select={() => selectCard(card)} />
+        {order.map((card) => {
+          return (
+            <Card
+              card={cards[card]}
+              key={card}
+              select={() => selectCard(card)}
+            />
+          );
         })}
       </section>
     </main>
