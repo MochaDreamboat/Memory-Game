f1e51a3d14c0ce6ebb3c11240ec4ceea0c31c8ae
mochadreamboat <jersotmac@gmail.com>
2022-10-30 22:50:25 -0500

    Added more cards to deck.

--- src/decks/overwatchDeck.js
+++ src/decks/overwatchDeck.js
@@ -15,6 +15,78 @@ let deck = {
         name: 'Baptiste',
         image: 'baptiste.png',
         picked: false
+    },
+
+    4: {
+        name: 'Bastion',
+        image: 'baptiste.png',
+        picked: false
+    },
+
+    5: {
+        name: 'Brigitte',
+        image: 'brigitte.png',
+        picked: false
+    },
+
+    6: {
+        name: 'Cassidy',
+        image: 'cassidy.png',
+        picked: false
+    },
+
+    7: {
+        name: 'Doomfist',
+        image: 'doomfist.png',
+        picked: false
+    },
+
+    8: {
+        name: 'D.Va',
+        image: 'dva.png',
+        picked: false
+    },
+
+    9: {
+        name: 'Echo',
+        image: 'echo.png',
+        picked: false
+    },
+
+    10: {
+        name: 'Genji',
+        image: 'genji.png',
+        picked: false
+    },
+
+    11: {
+        name: 'Hanzo',
+        image: 'hanzo.png',
+        picked: false
+    },
+
+    12: {
+        name: 'Junker Queen',
+        image: 'junker_queen.png',
+        picked: false
+    },
+
+    13: {
+        name: 'Junkrat',
+        image: 'junkrat.png',
+        picked: false
+    },
+
+    14: {
+        name: 'Kiriko',
+        image: 'kiriko.png',
+        picked: false
+    },
+
+    15: {
+        name: 'Lucio',
+        image: 'lucio.png',
+        picked: false
     }
 };
 
